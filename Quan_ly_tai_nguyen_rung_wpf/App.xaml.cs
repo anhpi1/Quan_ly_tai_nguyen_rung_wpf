031c03456b4277b3a5a2cf7b75b7edb871d55fcf
anhpi1 <phamhaianh660@gmail.com>
2024-11-16 17:28:43 +0700

    haianh

---
+++ Quan_ly_tai_nguyen_rung_wpf/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Quan_ly_tai_nguyen_rung_wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
