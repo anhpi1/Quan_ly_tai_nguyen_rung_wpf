0bd51b9e30246a9303570897bcb061795fa9db7a
anhpi1 <phamhaianh660@gmail.com>
2024-11-16 19:16:39 +0700

    haianh

---
+++ man_hinh_dang_nhap/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace man_hinh_dang_nhap
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
