5e164a3193eeb43a227ba129a4a3497f9a739cec
anhpi1 <phamhaianh660@gmail.com>
2024-11-19 15:15:07 +0700

    haianh

--- Quan_ly_tai_nguyen_rung_wpf/ctrl_test.xaml.cs
+++ Quan_ly_tai_nguyen_rung_wpf/ctrl_test.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Quan_ly_tai_nguyen_rung_wpf
 {
@@ -33,6 +34,32 @@ namespace Quan_ly_tai_nguyen_rung_wpf
         private void btn_ctrl_Click(object sender, RoutedEventArgs e)
         {
             txb_main.Text = "đã nhận được lệnh từ btn_ctrl";
+            // Chuỗi kết nối MySQL
+            string connectionString = "Server=localhost;Database=quan_li_rung;Uid=root;Pwd=y123456;";
+
+            // Kiểm tra kết nối
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open(); // Mở kết nối
+
+                    if (connection.State == System.Data.ConnectionState.Open)
+                    {
+                        MessageBox.Show("Kết nối thành công!", "Thông báo", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Không thể kết nối!", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+
+                    connection.Close(); // Đóng kết nối
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Lỗi khi kết nối: {ex.Message}", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
